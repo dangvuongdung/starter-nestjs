07fd81d532ef1dc838707d503c527a3f648048a9
Dang Vuong Dung <33083569+dangvuongdung@users.noreply.github.com>
2023-08-31 15:02:40 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import constant from './configs/constant';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Docs')
+      .setDescription('API description')
+      .setVersion(constant.swagger.version)
+      .addBearerAuth({ in: 'header', type: 'http' })
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(constant.swagger.pathName, app, document);
+  }
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
